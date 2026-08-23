35ef0433db955b3e5cdfadd387dcb4a27e883fa6
Dhanashree Virdande <dhanashreevirdande@gmail.com>
2026-08-23 20:03:53 +0530

    Hollow Rectangle Pattern

--- Pathollowrect.java
+++ Pathollowrect.java
@@ -9,6 +9,7 @@ public class Pathollowrect{
     public static void main(String args[]){
         int n=4;
         int m=5;
+        System.out.println("This is hollow rectanglt");
         for(int i = 1; i<=n;i++){
             for(int j =1;j<=m;j++){
                 if(i==1||j==1||i==n||j==m){
